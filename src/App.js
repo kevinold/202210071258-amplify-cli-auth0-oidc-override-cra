9a87089ad3a0a900405f8baec50e1cf123944029
Kevin Old <kevin@kevinold.com>
2022-10-18 15:16:22 -0500

    wip - scaffold initial App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify, API, Auth } from "aws-amplify";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import awsconfig from "./aws-exports";
+import { createTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
 
-function App() {
+Amplify.configure(awsconfig);
+
+function App({ user }) {
+  const [todos, setTodos] = useState([]);
+
+  async function getTodos() {
+    const { data } = await API.graphql({
+      query: listTodos,
+    });
+
+    return data;
+  }
+
+  async function onCreateTodo(user) {
+    await API.graphql({
+      query: createTodo,
+      variables: {
+        input: {
+          name: `New Todo ${user.email} - ${Date()}`,
+          description: `${user.email} - ${Date()}\n`,
+        },
+      },
+    });
+  }
+
+  useEffect(() => {
+    getTodos()
+      .then((data) => setTodos(data.listTodos.items))
+      .catch(() => console.log("error with todos"));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={() => Auth.signOut()}>Sign Out</button>
+      </div>
+      <div>User: {user.username} </div>
+      <div>
+        <button onClick={() => onCreateTodo(user) && getTodos()}>
+          Create Todo
+        </button>
+      </div>
+      <div>
+        <ul>{todos && todos.map((t, i) => <li key={i}>{t.name}</li>)}</ul>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
