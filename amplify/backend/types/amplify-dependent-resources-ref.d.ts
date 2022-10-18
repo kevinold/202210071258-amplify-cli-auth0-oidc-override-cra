1cee521c7013b5f498c8ded37eba4982ce6b2cea
Kevin Old <kevin@kevinold.com>
2022-10-18 15:10:27 -0500

    generate GraphQL API against cognito and set Todo with owner-base auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "202210071258amplifyc": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
     }
 }
