e10721265c5f8b11e72e1efb947eb08678882033
Kevin Old <kevin@kevinold.com>
2022-10-19 14:14:44 -0500

    add trailing slash

--- amplify/backend/auth/202210071258amplifyc729c9e0f/override.ts
+++ amplify/backend/auth/202210071258amplifyc729c9e0f/override.ts
@@ -7,14 +7,14 @@ export function override(resources: AmplifyAuthCognitoStackTemplate) {
     // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-cognito-userpoolclient.html
 
     resources.userPoolClient.addOverride("Properties.SupportedIdentityProviders", ["Auth0"])
-    resources.userPoolClient.addOverride("Properties.CallbackURLs", ["http://localhost:3000"])
-    resources.userPoolClient.addOverride("Properties.LogoutURLs", ["http://localhost:3000"])
+    resources.userPoolClient.addOverride("Properties.CallbackURLs", ["http://localhost:3000/"])
+    resources.userPoolClient.addOverride("Properties.LogoutURLs", ["http://localhost:3000/"])
     resources.userPoolClient.addOverride("Properties.AllowedOAuthFlows", ["code"])
     resources.userPoolClient.addOverride("Properties.AllowedOAuthScopes", allowedOAuthScopes)
 
     resources.userPoolClientWeb.addOverride("Properties.SupportedIdentityProviders", ["Auth0"])
-    resources.userPoolClientWeb.addOverride("Properties.CallbackURLs", ["http://localhost:3000"])
-    resources.userPoolClientWeb.addOverride("Properties.LogoutURLs", ["http://localhost:3000"])
+    resources.userPoolClientWeb.addOverride("Properties.CallbackURLs", ["http://localhost:3000/"])
+    resources.userPoolClientWeb.addOverride("Properties.LogoutURLs", ["http://localhost:3000/"])
     resources.userPoolClientWeb.addOverride("Properties.AllowedOAuthFlows", ["code"])
     resources.userPoolClientWeb.addOverride("Properties.AllowedOAuthScopes", allowedOAuthScopes)
 }
